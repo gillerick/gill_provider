52e1c8907d9d998f4d230cb2801045e6a90c8f10
Samuel Baraka <sababuvercetti@gmail.com>
2021-04-28 17:42:53 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/vercetti/provider_gill/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vercetti.provider_gill
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+// ignore_for_file: public_member_api_docs, lines_longer_than_80_chars
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => DataHolder())],
+      child: MaterialApp(
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(context.read<DataHolder>().name),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => Page2()));
+        },
+      ),
+      body: Center(
+        child: TextFormField(
+          onChanged: (val) => context.read<DataHolder>().change(name: val),
+        ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String name = context.read<DataHolder>().name;
+    return Scaffold(
+      body: Center(
+        child: Text(name),
+      ),
+    );
+  }
+}
+
+class DataHolder with ChangeNotifier, DiagnosticableTreeMixin {
+  String _name = "";
+
+  String get name => _name;
+  void change({String name}) {
+    _name = name;
+    notifyListeners();
+  }
+}
